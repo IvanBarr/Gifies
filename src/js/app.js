2ba7fe94b348494f75b6b18da89c51d460546469
IvanBarr <ivanbar.webdev@gmail.com>
2018-03-28 19:03:13 -0700

    fixed sideNav bug

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,8 @@ ourRequest.onload = function(){
 };
 ourRequest.send();
 
+// CREATES GIF CONTAINER AND PUTS THE GIF IN IT
 var gifWrapper = document.getElementById('gifWrapper');
-
 function createGif(data){
   for(var i = 0; i < data.data.length; i++){
 
@@ -27,9 +27,9 @@ function createGif(data){
   }
 }
 
+// LISTENES TO WHICH CATEGORY THE USER PICKED
 var categories = document.getElementById('categories');
 categories.addEventListener('click', pickCategory);
-
 function pickCategory(event){
   var target = event.target;
   var targetTextValue;
@@ -48,6 +48,7 @@ function pickCategory(event){
   }
 }
 
+// DELETES OLD GIFS
 function deleteOldContent(){
   var gifContainers = gifWrapper.querySelectorAll('.gifContainer');
   for(var x = 0; x < gifContainers.length; x++){
@@ -55,6 +56,7 @@ function deleteOldContent(){
   }
 }
 
+// UPDATES API KEY AND CREATES NEW GIFS
 function updateApiKey(){
   ourRequest.open('GET', api_key);
   ourRequest.onload = function(){
@@ -64,19 +66,25 @@ function updateApiKey(){
   ourRequest.send();
 }
 
-
+// LOOKS AT THE INPUT VALUE AND CHANGES THE API KEY
 var searchBtn = document.getElementsByClassName('searchBar_btn')[0];
+var searchInput = document.getElementById('searchInput');
 searchBtn.addEventListener('click', searchGifs);
 function searchGifs(){
-  var searchInput = document.getElementById('searchInput');
   var searchInputValue = searchInput.value;
-  api_key = 'http://api.giphy.com/v1/gifs/search?q=' + searchInputValue + '&api_key=8wEih3Gu7pXaPfNAWqBYhON7T8UTUFz9&limit=40';
-  title.innerHTML = searchInputValue;
-  deleteOldContent();
-  updateApiKey();
-  searchInput.value = '';
+
+  if(searchInputValue){
+    api_key = 'http://api.giphy.com/v1/gifs/search?q=' + searchInputValue + '&api_key=8wEih3Gu7pXaPfNAWqBYhON7T8UTUFz9&limit=40';
+    title.innerHTML = searchInputValue;
+    deleteOldContent();
+    updateApiKey();
+    searchInput.value = '';
+  }else if(searchInputValue === ''){
+    return;
+  }
 }
 
+// TAKES CARE OF THE SEARCH BAR FOR MOBILE DEVICES
 var searchBtnMobile = document.getElementsByClassName('search_btn')[0];
 var searchBar = document.getElementsByClassName('searchBar')[0];
 searchBtnMobile.addEventListener('click', openSearchInputMobile);
@@ -88,19 +96,34 @@ function openSearchInputMobile(){
     searchBar.style.width = '0';
     searchBar.style.boxShadow = '';
   }
-
 }
 
+searchInput.addEventListener('keypress', function(e){
+  var key = e.wich || e.keyCode;
+  if(key === 13){ //key code for 'Enter' is 13
+    searchGifs();
+  }
+});
+
+// REMOVES SearchBarMobile on Desktop
+window.addEventListener('resize', function(){
+  var screenWidth = window.innerwidth || document.documentElement.clientWidth;
+  if(screenWidth > 397){
+    searchBar.style.width = '';
+    searchBar.style.boxShadow = '';
+
+      sideNav.style.position = '';
+      sideNav.style.top = '';
+      sideNav.childNodes[1].style.paddingTop = '';
+  }
+});
 
 
 
+// MAKES NAV STICKY ON SCROLL
 var nav = document.getElementsByTagName('nav')[0];
 var sideNav = document.getElementsByClassName('sideNav')[0];
 window.onscroll = function(){stickyNav();};
-var mediaQueryMobile = window.matchMedia( "(max-width: 414px)" );
-
-
-// ADD MEDIA QUERIES TO THE SIDENAV POSITION
 function stickyNav(){
   var screenWidth = window.innerwidth || document.documentElement.clientWidth;
   if(window.pageYOffset >= 70){
@@ -122,8 +145,9 @@ function stickyNav(){
   }
 }
 
-var menuBtn = document.getElementById('menuBtn');
 
+// DROP DOWN MENU
+var menuBtn = document.getElementById('menuBtn');
 menuBtn.addEventListener('click', dropDownMenu);
 function dropDownMenu(){
   if(menuBtn.checked){
@@ -133,47 +157,9 @@ function dropDownMenu(){
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var screenWidth = window.innerwidth || document.documentElement.clientWidth;
-// // window.onload = function(){
-// //   if(screenWidth > 414){
-// //     sideNav.style.position = '';
-// //     sideNav.style.color = 'red';
-// //     sideNav.style.top = '';
-// //     sideNav.childNodes[1].style.paddingTop = '';
-// //   }else{
-// //     sideNav.style.color = '';
-// //   }
-// // }
-// //
-// // window.addEventListener('resize', function(){
-// //   var screenWidth = window.innerwidth || document.documentElement.clientWidth;
-// //   if(screenWidth > 414){
-// //     screenWidth = screenWidth;
-// //     sideNav.style.position = '';
-// //     sideNav.style.color = 'red';
-// //     sideNav.style.top = '';
-// //     sideNav.childNodes[1].style.paddingTop = '';
-// //   }else{
-// //     sideNav.style.color = '';
-// //   }
-// // })
+// FAVORITE
+var favoriteBtn = document.getElementsByClassName('favorite')[0];
+favoriteBtn.addEventListener('click', addToFavorite);
+function addToFavorite(){
+  alert('Feature coming soon!');
+}
